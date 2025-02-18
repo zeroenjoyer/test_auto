2eb0c91f06fe2c53bec540dcf0b82c6c88c38830
zeroenjoyer <solov.smn@gmail.com>
2025-02-18 12:13:30 +0300

    files 228

--- test_main_page.py
+++ test_main_page.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.by import By
 
 from .pages.main_page import MainPage
 
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
