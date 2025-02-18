9d4b5a833f7ae4b001d83003fb87b00be4d061a5
zeroenjoyer <solov.smn@gmail.com>
2025-02-18 17:03:10 +0300

    много всего

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,13 +2,17 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        # login_link = self.browser.find_element(By.CSS_SELECTOR, "#registration_link")
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url) для первого варианта перехода на новую вкладку
+        # alert = self.browser.switch_to.alert # если разраб даст ёбу и сделает после нажатия на кнопку алерт, то может крашнуться много тестов, поэтому одной строчкой можно это пофиксить - эт плюс патерна page object, чтобы не добавлять эту строчку во все тесты
+        # alert.accept()
     
     def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented" #Обратите внимание здесь на символ *, он указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать. 
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented" #Обратите внимание здесь на символ *, он указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать.
+        
